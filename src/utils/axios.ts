bb4728cdf1be6b34e94c1dca9dd3a3bba63edb67
rapwind <hayami_rikuo@cyberagent.co.jp>
2020-10-15 18:55:51 +0900

    Fix bugs

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,12 +1,10 @@
 import axios from "axios";
 
-const { MOCK, API_BASE_URL } = process.env;
-
 const axiosInstance = axios.create({
-  baseURL: API_BASE_URL || "/api",
+  baseURL: process.env.API_BASE_URL || "/api",
   timeout: 3000,
 });
-if (MOCK && MOCK === "true") {
+if (process.env.MOCK && process.env.MOCK === "true") {
   // eslint-disable-next-line @typescript-eslint/no-var-requires, global-require
   const mock = require("~/stubs/mock").default;
   mock(axiosInstance);
